228dabb7c86e6bbd0e1f293db3e376760a938126
Chris Terra <christerra@u.boisestate.edu>
2018-09-20 09:55:38 -0600

    Movement script prototype
    Createda script for 2d movement. Needs to be updated to current unity standards though as there are compiler errors due to old methods.

---
+++ Uneasy Allies/Assets/Scripts/Movement Script.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Quaternion rot = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.rotation = rot;
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+        rigidbody2D.angularVelocity = 0;
+        float input = Input.GetAxis("Vertical");
+        rigidbody2D.AddForce(gameObject.transform.up * speed * input);
+    }
+}

2996129dc95f2d25260d3dbae3591042a0f743c1
Chris Terra <christerra@u.boisestate.edu>
2018-11-11 15:34:04 -0700

    (internal Screaming)

---
+++ Uneasy Alliestest2.0/Assets/Scripts/LocalSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LocalSetup : NetworkBehaviour {
+
+	// Use this for initialization
+
+	public GameObject NotUs;
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer){
+			return;
+		}
+
+		NotUs.GetComponent<MovementScript> ().enabled= false;
+		NotUs.GetComponent<Gun> ().enabled= false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
